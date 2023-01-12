9677a031d1f37e16b87cba30b5cbfc05ebedca63
Adeshina Adenuga <nuga0909@gmail.com>
2023-01-12 08:51:08 +0100

    Add function to remove book

--- src/index.js
+++ src/index.js
@@ -24,3 +24,14 @@ function addBook(title, author) {
 addBook('book3', 'author3');
 
 console.log(books);
+
+function removeBook(id, arr) {
+  console.log(arr[id]);
+  arr.splice(id, 1);
+}
+
+removeBook(2, books);
+
+console.log(books);
+
+
