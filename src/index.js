3d62c90e8aef480b15e7095fea76422c2c92e581
Adeshina Adenuga <nuga0909@gmail.com>
2023-01-12 07:34:52 +0100

    Create addbook function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import _ from 'lodash';
 import './style.css';
 
+const books = [
+  {
+    title: 'book1',
+    author: 'author1'
+},
+{
+  title: 'book2',
+  author: 'author2'
+},
+];
+
+function addBook(title, author) {
+  const obj = {};
+  const newTitle = title;
+  const newAuthor = author;
+  obj['title'] = newTitle;
+  obj['author'] = newAuthor;
+  books.push(obj);
+}
+
+addBook('book3', 'author3');
+
+console.log(books);
